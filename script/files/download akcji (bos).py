86af3a21ff3f4c0d3b4db80fd9f1bccf7ea2e514
ZbiFi <zfietkiewicz@gmail.com>
2024-01-15 20:27:06 +0100

    disabled trypln download

--- script/files/download akcji (bos).py
+++ script/files/download akcji (bos).py
@@ -18,28 +18,28 @@ def main():
 
 def manage_download_all():
     filename = seleniumfolder + "ALL" + '.zip'
-    filename_2 = seleniumfolder + "TRYPLN" + '.csv'
+    # filename_2 = seleniumfolder + "TRYPLN" + '.csv'
     today = dt.datetime.now().date()
-    if os.path.exists(filename) and os.path.exists(filename_2):
+    if os.path.exists(filename):  # and os.path.exists(filename_2):
         filetime = dt.datetime.fromtimestamp(os.path.getmtime(filename))
-        filetime_2 = dt.datetime.fromtimestamp(os.path.getmtime(filename_2))
+        # filetime_2 = dt.datetime.fromtimestamp(os.path.getmtime(filename_2))
         if filetime.date() != today:
             os.remove(filename)
-        if filetime_2.date() != today:
-            os.remove(filename_2)
-        if filetime.date() == today and filetime_2.date() == today:
+        # if filetime_2.date() != today:
+        #     os.remove(filename_2)
+        if filetime.date() == today:  # and filetime_2.date() == today:
             return
 
     fullfilename = os.path.join(seleniumfolder, "ALL" + '.zip')
-    fullfilename_2 = os.path.join(seleniumfolder, "TRYPLN" + '.csv')
+    # fullfilename_2 = os.path.join(seleniumfolder, "TRYPLN" + '.csv')
     if not os.path.exists(seleniumfolder):
         os.makedirs(seleniumfolder)
         os.makedirs(seleniumfolder + "ALL")
     urllib.request.urlretrieve('http://bossa.pl/pub/metastock/mstock/mstall.zip', fullfilename)
     print("downloaded ALL")
 
-    urllib.request.urlretrieve('https://stooq.pl/q/d/l/?s=trypln&i=d', fullfilename_2)
-    print("downloaded TRYPLN")
+    # urllib.request.urlretrieve('https://stooq.pl/q/d/l/?s=trypln&i=d', fullfilename_2)
+    # print("downloaded TRYPLN")
     if os.path.exists(filename):
         zip_ref = zipfile.ZipFile(seleniumfolder + "ALL" + '.zip', 'r')
         zip_ref.extractall(seleniumfolder + "ALL")
