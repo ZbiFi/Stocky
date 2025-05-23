8ae19a913f75370a76a74fd0da74e502f6e04549
fietkiewiczz <kamos5@gazeta.pl>
2025-05-23 09:51:51 +0200

    commented some code in twitterAPI

--- script/files/TwitterAPI.py
+++ script/files/TwitterAPI.py
@@ -17,26 +17,26 @@ def makeSimpleTweet(text):
 
 def main(payload):
 
-    # Get request token
-    request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-    oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-    try:
-        fetch_response = oauth.fetch_request_token(request_token_url)
-    except ValueError:
-        print(
-            "There may have been an issue with the consumer_key or consumer_secret you entered."
-        )
-
-    resource_owner_key = fetch_response.get("oauth_token")
-    resource_owner_secret = fetch_response.get("oauth_token_secret")
-    print("Got OAuth token: %s" % resource_owner_key)
+    # # Get request token
+    # request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+    # oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+    #
+    # try:
+    #     fetch_response = oauth.fetch_request_token(request_token_url)
+    # except ValueError:
+    #     print(
+    #         "There may have been an issue with the consumer_key or consumer_secret you entered."
+    #     )
+    #
+    # resource_owner_key = fetch_response.get("oauth_token")
+    # resource_owner_secret = fetch_response.get("oauth_token_secret")
+    # print("Got OAuth token: %s" % resource_owner_key)
 
     # Get authorization
     base_authorization_url = "https://api.twitter.com/oauth/authorize"
-    authorization_url = oauth.authorization_url(base_authorization_url)
-    print("Please go here and authorize: %s" % authorization_url)
-    verifier = access_token
+    # authorization_url = oauth.authorization_url(base_authorization_url)
+    # print("Please go here and authorize: %s" % authorization_url)
+    # verifier = access_token
 
     # Make the request
     oauth = OAuth1Session(
@@ -88,7 +88,7 @@ def reduceMessage(text, textMode):
     counter = 1
 
     for record in text:
-        newMessage = str(record[1]) + ':' + str(round(record[3], 2)) + ':' + translateDescriptions(record[len(record)-1]) + '\n'
+        newMessage = str(record[1]) + ':' + str(round(float(record[3]), 2)) + ':' + translateDescriptions(record[len(record)-1]) + '\n'
         if len(message) + message.count('\n') + len(newMessage) + newMessage.count('\n') + 9 >= (255 + message.count('|')) * counter:
             if counter == 1:
                 message = '[' + str(counter) + '/' + '**' + ']' + '\n' + message
